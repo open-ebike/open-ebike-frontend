ba46b9e75101ce73b64a7f073f42ee3078c0d059
Florian Schwanz <florian.schwanz@interoberlin.de>
2025-09-15 00:19:44 +0200

    chore: add imprint page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,15 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { ImprintComponent } from './pages/imprint/imprint.component';
 
 export const routes: Routes = [
   {
     path: 'home',
     component: HomeComponent,
+  },
+  {
+    path: 'imprint',
+    component: ImprintComponent,
   },
   { path: '**', redirectTo: 'home' },
 ];
---
+++ src/app/pages/imprint/imprint.component.ts
@@ -0,0 +1,101 @@
+import { Component, inject, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { combineLatest, first } from 'rxjs';
+import { getBrowserLang, TranslocoModule } from '@jsverse/transloco';
+import { Theme, ThemeService } from '../../services/theme.service';
+
+/**
+ * Displays imprint component
+ */
+@Component({
+  selector: 'app-imprint',
+  templateUrl: './imprint.component.html',
+  styleUrls: ['./imprint.component.scss'],
+  imports: [TranslocoModule],
+  standalone: true,
+})
+export class ImprintComponent implements OnInit {
+  //
+  // Injections
+  //
+
+  /** Activated route */
+  private route = inject(ActivatedRoute);
+  /** Router */
+  private router = inject(Router);
+  /** Theme service */
+  private themeService = inject(ThemeService);
+
+  /** Language */
+  lang = getBrowserLang();
+
+  //
+  // Constants
+  //
+
+  /** Query parameter theme */
+  private QUERY_PARAM_THEME: string = 'theme';
+
+  //
+  // Lifecycle hooks
+  //
+
+  /**
+   * Handles on-init phase
+   */
+  ngOnInit() {
+    this.initializeTheme();
+    this.handleQueryParameters();
+  }
+
+  //
+  // Initialization
+  //
+
+  /**
+   * Initializes theme
+   */
+  private initializeTheme() {
+    switch (this.themeService.theme()) {
+      case Theme.LIGHT: {
+        this.updateQueryParameters();
+        break;
+      }
+      case Theme.DARK: {
+        this.updateQueryParameters();
+        break;
+      }
+    }
+  }
+
+  /**
+   * Handles query parameters
+   */
+  private handleQueryParameters() {
+    combineLatest([this.route.queryParams])
+      .pipe(first())
+      .subscribe(([queryParams]) => {
+        const theme = queryParams[this.QUERY_PARAM_THEME];
+
+        this.themeService.switchTheme(theme ? theme : Theme.LIGHT);
+      });
+  }
+
+  //
+  // Helpers
+  //
+
+  /**
+   * Updates query parameters
+   */
+  private updateQueryParameters() {
+    this.router
+      .navigate([], {
+        relativeTo: this.route,
+        queryParams: {
+          [this.QUERY_PARAM_THEME]: this.themeService.theme(),
+        },
+      })
+      .then();
+  }
+}
