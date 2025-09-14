bb8cf9e0afb0912b16efbf5063146cf0181110a5
Florian Schwanz <florian.schwanz@interoberlin.de>
2025-09-14 22:51:38 +0200

    chore: add home component #1

---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+/**
+ * Displays home component
+ */
+@Component({
+  selector: 'app-home',
+  imports: [],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss',
+  standalone: true,
+})
+export class HomeComponent {}
