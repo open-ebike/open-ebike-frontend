caf77024206f626778ca63e4eb85610fedfed4b5
Florian Schwanz <florian.schwanz@interoberlin.de>
2025-09-16 07:57:42 +0200

    chore: add eBikes page to footer #3

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,17 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { getBrowserLang, TranslocoModule } from '@jsverse/transloco';
+import { AuthenticationService } from '../../services/authentication.service';
+import { RouterLink } from '@angular/router';
 
 /**
  * Displays footer
  */
 @Component({
   selector: 'app-footer',
-  imports: [TranslocoModule],
+  imports: [TranslocoModule, RouterLink],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss',
   standalone: true,
 })
 export class FooterComponent {
+  //
+  // Injections
+  //
+
+  /** Authentication service */
+  public authenticationService = inject(AuthenticationService);
+
   /** Current year */
   year = new Date().getFullYear();
 
