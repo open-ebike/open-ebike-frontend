8f7eff9f2c0271ace1034fcfeaa1ff6ef62e028b
Florian Schwanz <florian.schwanz@interoberlin.de>
2025-09-29 02:51:27 +0200

    chore: add environment config for deployment target GitHub pages #14

---
+++ src/environments/environment.github-pages.ts
@@ -0,0 +1,18 @@
+export const environment = {
+  production: false,
+  eBikeApiUrl: 'https://api.bosch-ebike.com',
+  authConfig: {
+    issuer: 'https://p9.authz.bosch.com/auth/realms/obc',
+    tokenEndpoint:
+      'https://p9.authz.bosch.com/auth/realms/obc/protocol/openid-connect/token',
+    authorizationEndpoint:
+      'https://p9.authz.bosch.com/auth/realms/obc/protocol/openid-connect/auth',
+    redirectUri: 'https://open-ebike.github.io/open-ebike-frontend',
+    clientId: 'euda-f6421696-34c8-44c6-9f43-c40e1ae704e4',
+    scope: 'euda:read',
+    responseType: 'code',
+    requireHttps: false,
+    strictDiscoveryDocumentValidation: false,
+    useSilentRefresh: true,
+  },
+};
