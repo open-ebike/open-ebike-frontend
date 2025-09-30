91266fd6fc46cd6e4c3629102061f6a484bda4b5
Florian Schwanz <florian.schwanz@interoberlin.de>
2025-09-30 21:20:46 +0200

    chore: add environment config for deployment target Firebase hosting #15

---
+++ src/environments/environment.firebase-hosting.ts
@@ -0,0 +1,18 @@
+export const environment = {
+  production: false,
+  eBikeApiUrl: 'https://api.bosch-ebike.com',
+  authConfig: {
+    issuer: 'https://p9.authz.bosch.com/auth/realms/obc',
+    tokenEndpoint:
+      'https://p9.authz.bosch.com/auth/realms/obc/protocol/openid-connect/token',
+    authorizationEndpoint:
+      'https://p9.authz.bosch.com/auth/realms/obc/protocol/openid-connect/auth',
+    redirectUri: 'https://open-ebike.web.app',
+    clientId: 'euda-2677687f-bed4-4083-982c-8e4a410b62ad',
+    scope: 'euda:read',
+    responseType: 'code',
+    requireHttps: false,
+    strictDiscoveryDocumentValidation: false,
+    useSilentRefresh: true,
+  },
+};
