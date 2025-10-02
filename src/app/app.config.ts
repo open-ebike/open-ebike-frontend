84dca96b6d953770758cc5d2d3ea2f32e59fc5b8
Florian Schwanz <florian.schwanz@interoberlin.de>
2025-10-02 19:31:10 +0200

    chore: remove OAuth configuration during bootstrapping #17

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,4 @@
 import {
-  APP_INITIALIZER,
   ApplicationConfig,
   importProvidersFrom,
   provideZoneChangeDetection,
@@ -15,8 +14,7 @@ import {
 import { TranslocoRootModule } from './transloco-root.module';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
-import { OAuthService, provideOAuthClient } from 'angular-oauth2-oidc';
-import { environment } from '../environments/environment';
+import { provideOAuthClient } from 'angular-oauth2-oidc';
 import { AuthInterceptor } from './services/auth/auth-interceptor';
 
 export const appConfig: ApplicationConfig = {
@@ -32,21 +30,5 @@ export const appConfig: ApplicationConfig = {
     },
     provideAnimations(),
     provideOAuthClient(),
-    {
-      provide: 'AUTH_CONFIG',
-      useValue: environment.authConfig,
-    },
-    {
-      provide: APP_INITIALIZER,
-      useFactory: (oauthService: OAuthService) => {
-        return () => {
-          oauthService.configure(environment.authConfig);
-          oauthService.setupAutomaticSilentRefresh();
-          return oauthService.loadDiscoveryDocumentAndTryLogin();
-        };
-      },
-      deps: [OAuthService],
-      multi: true,
-    },
   ],
 };
