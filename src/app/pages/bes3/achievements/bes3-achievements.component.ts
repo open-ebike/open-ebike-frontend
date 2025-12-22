b9f434153d10c7d5b8434456458c29d39140641b
Florian Schwanz <florian.schwanz@interoberlin.de>
2025-12-22 02:02:23 +0100

    chore: add sample achievements for completed activities #36

--- src/app/pages/bes3/achievements/bes3-achievements.component.ts
+++ src/app/pages/bes3/achievements/bes3-achievements.component.ts
@@ -1,14 +1,33 @@
 import { Component, inject } from '@angular/core';
-import { AchievementService } from '../../../services/other/bes3/achievement.service';
+import {
+  AchievementService,
+  AchievementType,
+} from '../../../services/other/bes3/achievement.service';
 import { AuthenticationService } from '../../../services/authentication.service';
 import { getBrowserLang, TranslocoDirective } from '@jsverse/transloco';
+import { MatCard } from '@angular/material/card';
+import { MatRipple } from '@angular/material/core';
+import {
+  DatePipe,
+  KeyValuePipe,
+  NgClass,
+  TitleCasePipe,
+} from '@angular/common';
 
 /**
  * Displays achievements
  */
 @Component({
   selector: 'app-achievements',
-  imports: [TranslocoDirective],
+  imports: [
+    TranslocoDirective,
+    KeyValuePipe,
+    MatCard,
+    NgClass,
+    MatRipple,
+    DatePipe,
+    TitleCasePipe,
+  ],
   templateUrl: './bes3-achievements.component.html',
   styleUrl: './bes3-achievements.component.scss',
   standalone: true,
@@ -21,4 +40,7 @@ export class Bes3AchievementsComponent {
 
   /** Language */
   lang = getBrowserLang();
+
+  /** Achievement type enum */
+  achievementTypeEnum = AchievementType;
 }
--- src/app/services/other/bes3/achievement.service.ts
+++ src/app/services/other/bes3/achievement.service.ts
@@ -1,8 +1,25 @@
 import { inject, Injectable } from '@angular/core';
-import {
-  ActivityRecordsService,
-  ActivitySummary,
-} from '../../api/bes3/activity-records.service';
+import { ActivityRecordsService } from '../../api/bes3/activity-records.service';
+
+/**
+ * Types of achievements
+ */
+export enum AchievementType {
+  ONE_ACTIVITY_COMPLETED = 'ONE_ACTIVITY_COMPLETED',
+  TEN_ACTIVITIES_COMPLETED = 'TEN_ACTIVITIES_COMPLETED',
+}
+
+/**
+ * Represents an achievement
+ */
+export interface Achievement {
+  /** Date */
+  date?: string;
+  /** Icon */
+  icon?: string;
+  /** Translation */
+  translation?: string;
+}
 
 /**
  * Handles achievements
@@ -11,31 +28,79 @@ import {
   providedIn: 'root',
 })
 export class AchievementService {
-  /** Activities */
-  private activitySummaries: ActivitySummary[] = [];
+  //
+  // Injections
+  //
 
   /** Activity records service */
   private activityRecordsService = inject(ActivityRecordsService);
+
+  /** Achievements and their date of achieval */
+  achievements = new Map<AchievementType, Achievement>([
+    [
+      AchievementType.ONE_ACTIVITY_COMPLETED,
+      {
+        icon: 'assets/achievements/medal.png',
+        translation: 'terms.one-activity-completed',
+      },
+    ],
+    [
+      AchievementType.TEN_ACTIVITIES_COMPLETED,
+      {
+        icon: 'assets/achievements/medal.png',
+        translation: 'terms.ten-activities-completed',
+      },
+    ],
+  ]);
 
   /**
    * Constructor
    */
   constructor() {
-    this.loadActivities();
+    this.initialize();
   }
 
   //
   // Helpers
   //
 
+  /** Total activity count */
+  totalActivityCount = 0;
+
   /**
-   * Loads activities
+   * Loads activities and evaluates if achievements have been reached
    */
-  private async loadActivities() {
+  private async initialize() {
     this.activityRecordsService
-      .getAllActivitySummariesRecursively(100)
+      .getAllActivitySummariesRecursively(100, 'startTime')
       .subscribe((activitySummaries) => {
-        this.activitySummaries = activitySummaries;
+        activitySummaries.forEach((activitySummary) => {
+          this.totalActivityCount += 1;
+
+          if (
+            !this.achievements.get(AchievementType.ONE_ACTIVITY_COMPLETED)
+              ?.date &&
+            this.totalActivityCount >= 1
+          ) {
+            this.achievements.set(AchievementType.ONE_ACTIVITY_COMPLETED, {
+              ...this.achievements.get(AchievementType.ONE_ACTIVITY_COMPLETED),
+              date: activitySummary.endTime,
+            });
+          }
+
+          if (
+            !this.achievements.get(AchievementType.TEN_ACTIVITIES_COMPLETED)
+              ?.date &&
+            this.totalActivityCount >= 10
+          ) {
+            this.achievements.set(AchievementType.TEN_ACTIVITIES_COMPLETED, {
+              ...this.achievements.get(
+                AchievementType.TEN_ACTIVITIES_COMPLETED,
+              ),
+              date: activitySummary.endTime,
+            });
+          }
+        });
       });
   }
 }
