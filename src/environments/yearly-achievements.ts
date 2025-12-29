36429fdb48a24fff215735ec325b54e892f62150
Florian Schwanz <florian.schwanz@interoberlin.de>
2025-12-29 15:46:43 +0100

    chore: add yearly achievements #38

---
+++ src/environments/yearly-achievements.ts
@@ -0,0 +1,34 @@
+/**
+ * Types of yearly achievements
+ */
+export enum YearlyAchievementType {
+  TOTAL_ACTIVITY_COUNT = 'TOTAL_ACTIVITY_COUNT',
+  TOTAL_DISTANCE = 'TOTAL_DISTANCE',
+  TOTAL_ELEVATION_GAIN = 'TOTAL_ELEVATION_GAIN',
+  TOTAL_CALORIES_BURNED = 'TOTAL_CALORIES_BURNED',
+}
+
+export const yearlyAchievements = {
+  general: [
+    {
+      type: YearlyAchievementType.TOTAL_ACTIVITY_COUNT,
+      icon: 'assets/achievements/mobile.png',
+      translationSharePicture: 'terms.share-pictures.total-activity-count',
+    },
+    {
+      type: YearlyAchievementType.TOTAL_DISTANCE,
+      icon: 'assets/achievements/pathway.png',
+      translationSharePicture: 'terms.share-pictures.total-distance',
+    },
+    {
+      type: YearlyAchievementType.TOTAL_ELEVATION_GAIN,
+      icon: 'assets/achievements/success.png',
+      translationSharePicture: 'terms.share-pictures.total-elevation-gain',
+    },
+    {
+      type: YearlyAchievementType.TOTAL_CALORIES_BURNED,
+      icon: 'assets/achievements/burn.png',
+      translationSharePicture: 'terms.share-pictures.total-calories-burned',
+    },
+  ],
+};
