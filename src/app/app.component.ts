c921f85a407ce5338a77cd6961f61417c2a17ba9
Florian Schwanz <florian.schwanz@interoberlin.de>
2025-12-30 14:49:39 +0100

    fix: un-themed bottom sheet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, effect, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ThemeService } from './services/theme.service';
+import { Theme, ThemeService } from './services/theme.service';
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { environment } from '../environments/environment';
 import { AuthenticationService } from './services/authentication.service';
+import { Meta } from '@angular/platform-browser';
+import { OverlayContainer } from '@angular/cdk/overlay';
 
 /**
  * Displays app component
@@ -23,13 +25,44 @@ export class AppComponent implements OnInit {
 
   /** Theme service */
   public themeService = inject(ThemeService);
+  /** Overlay container */
+  private overlayContainer = inject(OverlayContainer);
   /** Authentication service */
   public authenticationService = inject(AuthenticationService);
+  /** Meta service */
+  private meta = inject(Meta);
 
+  /**
+   * Constructor
+   */
   constructor() {
     if (environment.mapbox.accessToken) {
       localStorage.setItem('mapboxAccessToken', environment.mapbox.accessToken);
     }
+
+    effect(() => {
+      // Theme menus and dialogs
+      const overlayContainerClasses =
+        this.overlayContainer.getContainerElement().classList;
+      const themeClassesToRemove = Array.from(overlayContainerClasses).filter(
+        (item: string) => item.includes('theme'),
+      );
+      if (themeClassesToRemove.length) {
+        overlayContainerClasses.remove(...themeClassesToRemove);
+      }
+      overlayContainerClasses.add(this.themeService.theme());
+
+      switch (this.themeService.theme()) {
+        case Theme.LIGHT: {
+          this.meta.updateTag({ content: '#FFFFFF' }, 'name=theme-color');
+          break;
+        }
+        case Theme.DARK: {
+          this.meta.updateTag({ content: '#212121' }, 'name=theme-color');
+          break;
+        }
+      }
+    });
   }
 
   //
@@ -41,5 +74,19 @@ export class AppComponent implements OnInit {
    */
   async ngOnInit() {
     await this.authenticationService.restoreConfig();
+    this.initializeTheme();
+  }
+
+  //
+  // Initialization
+  //
+
+  /**
+   * Initializes theme
+   */
+  private initializeTheme() {
+    this.overlayContainer
+      .getContainerElement()
+      .classList.add(this.themeService.theme());
   }
 }
